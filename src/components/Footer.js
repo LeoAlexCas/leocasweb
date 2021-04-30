d65960e22d63217edc493e5ae1a01cdc7560cef3
Leonardo Castro <castroleonardo@live.cl>
2021-04-29 21:34:12 -0400

    creado footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,10 @@ class Footer extends React.Component {
     render() {
         return(
             <div className="footerContainer">
-                
+                <p>Este es el footer</p>
             </div>
         );
     }
 }
+
+export default Footer;
