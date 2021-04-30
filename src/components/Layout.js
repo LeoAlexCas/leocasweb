5f3f626315fbbe628ef5bdb5ed8e85b83cf0ef0c
Leonardo Castro <castroleonardo@live.cl>
2021-04-29 21:28:29 -0400

    generando componentes del home

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 class Layout extends React.Component {
     render() {
@@ -7,6 +8,7 @@ class Layout extends React.Component {
             <React.Fragment>
                 <NavBar></NavBar>
                 {this.props.children}
+                <Footer></Footer>
             </React.Fragment>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,18 @@ import React from 'react';
 class NavBar extends React.Component {
     render() {
         return(
-            <div>
-                <p>esto es el navbar</p>
+            <div className="navContainer">
+                <div className="navLogo">
+                    <img src="" alt=""/>
+                </div>
+                <div className="navMenu">
+                    <ul>
+                        <li>uno</li>
+                        <li>dos</li>
+                        <li>tres</li>
+                        <li>4</li>
+                    </ul>
+                </div>
             </div>
         );
     }
